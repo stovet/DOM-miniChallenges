71eb80751854a388ab42bc9ab8c3bbd6f3c8f259
stovet <steven.topalian@gmail.com>
2021-11-30 15:54:34 -0500

    eventListeners added

---
+++ script.js
@@ -0,0 +1,28 @@
+let colaBtn = document.getElementById("cola");
+let peanutsBtn = document.getElementById("peanuts");
+let chocolateBtn = document.getElementById("chocolate");
+let gummyBtn = document.getElementById("gummies");
+let h1 = document.querySelector("h1");
+let total = 0;
+
+colaBtn.addEventListener('click', colaClick)
+peanutsBtn.addEventListener('click', peanutsClick)
+chocolateBtn.addEventListener('click', chocolateClick)
+gummyBtn.addEventListener('click', gummyClick);
+
+function colaClick(){
+    total += 2;
+    h1.innerText = `Total $${total}.00`
+}
+function peanutsClick(){
+    total += 3;
+    h1.innerText = `Total $${total}.00`
+}
+function chocolateClick(){
+    total += 4;
+    h1.innerText = `Total $${total}.00`
+}
+function gummyClick(){
+    total += 6;
+    h1.innerText = `Total $${total}.00`
+}
